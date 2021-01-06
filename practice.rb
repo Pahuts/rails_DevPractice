c1f593fabde43048a22e6f79936e6d5be225c2d8
Pahuts <luispahutanblock@gmail.com>
2021-01-06 21:00:24 +0800

    added practice.rb file

---
+++ practice.rb
@@ -0,0 +1,10 @@
+# puts 2+2
+# puts 5 - 2
+# puts 5 * 3
+# puts 20.5 / 10
+
+# puts Math.sqrt(4) # calculate the square root using the math module, a prewritten chunk of code
+
+#puts "My Name is" # this is a string
+
+puts "My Name is, " + "Ronald"
